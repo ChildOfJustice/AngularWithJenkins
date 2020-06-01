7ed38083683ffaba62c9337ae41d7d69d3873adc
ChildOfJustice <51397216+ChildOfJustice@users.noreply.github.com>
2020-06-01 13:39:03 +0300

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,7 +5,7 @@ sudo git clone https://github.com/ChildOfJustice/AngularWithJenkins.git
 sudo cp -R ./AngularWithJenkins/dist/mixTestApp/* /var/www/html/
 sudo rm -rf AngularWithJenkins/
 
-sudo /etc/init.d/apache2 start
+#sudo /etc/init.d/apache2 start
 
-echo 'Now...'
+echo 'Now start the apache server on your EC2: $ sudo service httpd start'
 echo 'Visit http://<PUBLIC IP>/ to see your application in action.'
